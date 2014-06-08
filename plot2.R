f1b5e1665f83d0a6a63daad7a3af4d85fc339bb4
Vijay Chopra <vchops75@gmail.com>
2014-06-08 19:14:55 -0400

    Final changes

--- plot2.R
+++ plot2.R
@@ -3,6 +3,6 @@ powerconsume <- fread("./data/household_power_consumption.txt",colClasses="chara
 subpower <- subset(powerconsume,Date%in% c('1/2/2007','2/2/2007'))
 subpower[, DateTime := as.character(strptime(paste(subpower$Date, subpower$Time), format = "%d/%m/%Y %H:%M:%S"))]
 library(datasets)
-png(file = "plot1.png", bg = "WHITE", width = 480, height = 480, units = "px")
+png(file = "plot2.png", bg = "WHITE", width = 480, height = 480, units = "px")
 plot(as.POSIXlt(subpower$DateTime),as.numeric(subpower$Global_active_power),type="l",xlab="",ylab="Global Active Power (kilowatts)")
 dev.off()
--- plot3.R
+++ plot3.R
@@ -3,7 +3,7 @@ powerconsume <- fread("./data/household_power_consumption.txt",colClasses="chara
 subpower <- subset(powerconsume,Date%in% c('1/2/2007','2/2/2007'))
 subpower[, DateTime := as.character(strptime(paste(subpower$Date, subpower$Time), format = "%d/%m/%Y %H:%M:%S"))]
 library(datasets)
-png(file = "plot2.png", bg = "WHITE", width = 480, height = 480, units = "px")
+png(file = "plot3.png", bg = "WHITE", width = 480, height = 480, units = "px")
 plot(as.POSIXlt(subpower$DateTime),as.numeric(subpower$Sub_metering_1),type="l",xlab="",ylab="Energy sub metering")
 lines(as.POSIXlt(subpower$DateTime),as.numeric(subpower$Sub_metering_2),col="red")
 lines(as.POSIXlt(subpower$DateTime),as.numeric(subpower$Sub_metering_3),col="blue")
